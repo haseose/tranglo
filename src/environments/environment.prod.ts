3aed93c0063e78876509ccc63499e0e19e7e2be6
haseose <haseo2408@gmail.com>
2026-04-03 00:56:26 +0800

    Add environment file entries to .gitignore and update exchangeRateApiKey in environment.prod.ts; create vercel.json for CORS headers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  exchangeRateApiKey: 'YOUR_API_KEY_HERE',
+  exchangeRateApiKey: '1c0ee1aa62f7de4140164196',
   exchangeRateApiBaseUrl: 'https://v6.exchangerate-api.com/v6',
 };
